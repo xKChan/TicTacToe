8753cd6fe7109fc76b8bb0ff6f81b7ec93a48e5b
kc <xkylechan@gmail.com>
2023-08-03 17:52:50 -0600

    Let player click to add X marker on grid

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const Players = (name, marker) => {
 
 // Create Gameboard --> Module Function
 const gameboard = (() => {
-  let board = ["X", "X", "O", "O", "X", "O", "X", "O", "X"];
+  let board = ["", "", "", "", "", "", "", "", ""];
 
   // display board
   let displayBoard = document.querySelector(".gameBoard");
@@ -16,7 +16,30 @@ const gameboard = (() => {
     squares.textContent = board[i];
     displayBoard.appendChild(squares);
   }
+
   return {
     board,
+  };
+})();
+
+const gameController = (() => {
+  const playerOne = Players("Player 1", "X");
+  const playerTwo = Players("Player 2", "O");
+
+  function addGamePiece() {
+    document.querySelectorAll(".squares").forEach((square) => {
+      square.addEventListener("click", () => {
+        for (let i = 0; i < gameboard.board.length; i++) {
+          console.log("hello");
+          square.textContent = playerOne.marker;
+          return;
+        }
+      });
+    });
+  }
+
+  addGamePiece();
+  return {
+    addGamePiece,
   };
 })();
