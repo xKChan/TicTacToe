057b4159e92380d5b05cf1fbc3b488d5fa1070ab
kc <xkylechan@gmail.com>
2023-08-03 17:09:45 -0600

    Created game outline, with set array and css to style grid to show proper gameboard

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
-// Create Gameboard --> Module
-const gameboard = (() => {
-  board = ["", "", "", "", "", "", "", "", ""];
-})();
-
-// Create players --> Factory
+// Create PLayer --> Factory Function
 const Players = (name, marker) => {
-  const getName = () => name;
-  const getMarker = () => marker;
-
-  return { getName, getMarker };
+  return { name, marker };
 };
+
+// Create Gameboard --> Module Function
+const gameboard = (() => {
+  let board = ["X", "X", "O", "O", "X", "O", "X", "O", "X"];
+
+  // display board
+  let displayBoard = document.querySelector(".gameBoard");
+  for (let i = 0; i < board.length; i++) {
+    let squares = document.createElement("div");
+    squares.setAttribute("class", "squares");
+    squares.setAttribute("data-spot", i);
+    squares.textContent = board[i];
+    displayBoard.appendChild(squares);
+  }
+  return {
+    board,
+  };
+})();
