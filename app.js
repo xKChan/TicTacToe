04d4dd0f24486c8287928027f9e1553a3da69826
KC <xkylechan@gmail.com>
2023-08-05 19:20:04 -0600

    Alternate turns placing marker on board, displaying properly on DOM

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Create PLayer --> Factory Function
+// Create Player --> Factory Function
 const Players = (name, marker) => {
   return { name, marker };
 };
@@ -25,5 +25,33 @@ const gameboard = (() => {
 const gameController = (() => {
   const playerOne = Players("Player 1", "X");
   const playerTwo = Players("Player 2", "O");
-  let turn = 9;
+
+  const playerFirst = true;
+  let turns = 9;
+
+  const makeMove = () => {
+    playerMove = document.querySelectorAll(".squares");
+    playerMove.forEach((square) => {
+      square.addEventListener("click", (e) => {
+        square.textContent = whosTurn(playerFirst);
+        // console.log(e.target.getAttribute("data-spot"));
+      });
+    });
+  };
+
+  const whosTurn = () => {
+    if (turns % 2 === 1) {
+      console.log("p1");
+      turns--;
+      console.log(turns);
+      return playerOne.marker;
+    } else {
+      console.log("p2");
+      turns--;
+      console.log(turns);
+      return playerTwo.marker;
+    }
+  };
+
+  makeMove();
 })();
