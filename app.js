ecc6b2ee9c806bc7eb792e20b5c99ed3a93bd1ef
KC <xkylechan@gmail.com>
2023-08-09 00:56:03 -0600

    Added status to display winner/whos turn.

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const Players = (name, marker) => {
 // Create Gameboard --> Module Function
 const gameboard = (() => {
   let board = ["", "", "", "", "", "", "", "", ""];
+  let gameStatus = document.querySelector(".gameStatus");
 
   // display board
   let displayBoard = document.querySelector(".gameBoard");
@@ -19,48 +20,79 @@ const gameboard = (() => {
 
   return {
     board,
+    gameStatus,
   };
 })();
 
 const gameController = (() => {
-  const playerOne = Players("Player 1", "X");
-  const playerTwo = Players("Player 2", "O");
-  let currentPlayer;
+  let currentPlayer = "X";
   let turns = 9;
+  let running = false;
 
   const playGame = () => {
     playerMove = document.querySelectorAll(".squares");
-    playerMove.forEach((square) => {
-      square.addEventListener("click", () => {
-        whosTurn();
-        makeMove(square);
-        checkWinner();
-
-        // console.log(e.target.getAttribute("data-spot"));
+    running = true;
+    gameboard.gameStatus.textContent = `${currentPlayer}'s turn`;
+    if (running) {
+      playerMove.forEach((square) => {
+        square.addEventListener("click", () => {
+          makeMove(square);
+          whosTurn();
+          if (turns < 5) {
+            checkWinner();
+          }
+          checkTurns();
+          console.log(gameboard.board);
+        });
       });
-    });
-  };
-
-  const whosTurn = () => {
-    if (turns % 2 === 1) {
-      console.log("p1");
-      turns--;
-      console.log(turns);
-      currentPlayer = playerOne.marker;
-    } else {
-      console.log("p2");
-      turns--;
-      console.log(turns);
-      currentPlayer = playerTwo.marker;
     }
   };
 
+  const whosTurn = () => {
+    currentPlayer = currentPlayer == "X" ? "O" : "X";
+    gameboard.gameStatus.textContent = `${currentPlayer}'s turn`;
+  };
+
   const makeMove = (square) => {
-    square.classList.add("placed");
-    return (square.textContent = currentPlayer);
+    const spotClicked = square.getAttribute("data-spot");
+    console.log(spotClicked);
+    gameboard.board[spotClicked] = currentPlayer;
+    square.classList.add("placed", currentPlayer);
+    square.textContent = currentPlayer;
   };
 
   const checkWinner = () => {
+    const winConditions = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    let gamewinner = false;
+
+    for (let i = 0; i < winConditions.length; i++) {
+      const conditions = winConditions[i];
+      const cellA = gameboard.board[conditions[0]];
+      const cellB = gameboard.board[conditions[1]];
+      const cellC = gameboard.board[conditions[2]];
+
+      if (cellA == "" || cellB == "" || cellC == "") {
+        continue;
+      } else if (cellA == cellB && cellB == cellC) {
+        gamewinner = true;
+        break;
+      }
+    }
+    if (gamewinner) {
+      gameboard.gameStatus.textContent = `${currentPlayer} Wins!`;
+    }
+  };
+
+  const checkTurns = () => {
     if (turns == 0) {
       setTimeout(function () {
         alert("Game Over, No one wins!");
